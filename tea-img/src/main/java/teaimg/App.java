6eee61d15d21acf5e4dff40d7617a7f720d4acf3
daru <mica.tui@proton.me>
2024-05-21 06:25:59 +0200

    java: add sources

---
+++ tea-img/src/main/java/teaimg/App.java
@@ -0,0 +1,14 @@
+package teaimg;
+
+import org.apache.commons.cli.*;
+
+public class App {
+  public static void main(String[] args) {
+  }
+  static void getImage(){
+
+  }
+  static void convert(){
+
+  }
+}
