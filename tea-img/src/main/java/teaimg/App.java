92a2cdbf63741bfaac08eb52333f651520772af7
daru <mica.tui@proton.me>
2024-05-21 07:49:42 +0200

    options: add initial options

--- tea-img/src/main/java/teaimg/App.java
+++ tea-img/src/main/java/teaimg/App.java
@@ -5,10 +5,46 @@ import org.apache.commons.cli.*;
 public class App {
   public static void main(String[] args) {
   }
-  static void getImage(){
+
+  static void options() {
+    Options opts = new Options();
+    Option help = new Option("Help", "help", false, "Show the help menu");
+
+    Option image = Option
+        .builder("i")
+        .longOpt("image")
+        .argName("image")
+        .hasArg()
+        .required(true)
+        .desc("Image file name").build();
+
+    Option fromType = Option
+        .builder("f")
+        .longOpt("from")
+        .argName("string")
+        .hasArg()
+        .required(true)
+        .desc("Type to convert from (can be detected automatically if not specified)").build();
+
+    Option toType = Option
+        .builder("t")
+        .longOpt("to")
+        .argName("string")
+        .hasArg()
+        .required(true)
+        .desc("Type to conver to").build();
+
+    opts.addOption(help);
+    opts.addOption(image);
+    opts.addOption(fromType);
+    opts.addOption(toType);
+  }
+
+  static void getImage() {
 
   }
-  static void convert(){
+
+  static void convert() {
 
   }
 }
